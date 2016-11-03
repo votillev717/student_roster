9e2d6aaf68db7f3a301607edc072001b80ea2bc9
Evan Votilla <votillev717@gmail.com>
2016-11-03 01:05:33 -0600

    Made some new validations for fields within the Rails app.
    [#133071901]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :cell_phone, :presence => true
+  validates :cell_phone, :length => { :minimum => 12, :maximum => 12 }
+  validates :home_phone, :presence => true
+  validates :home_phone, :length => { :minimum => 12, :maximum => 12 }
+  validates :work_phone, :presence => true
+  validates :work_phone, :length => { :minimum => 12, :maximum => 12 }
+  validates :email, :presence => true
+  validates :email, :length => {:minimum => 4, :maximum => 50}
+  validates :email, :uniqueness => true
 end
