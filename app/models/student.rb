5e3c077c0b4c24735ea89b513f95a204a3b2f4c0
Evan Votilla <votillev717@gmail.com>
2016-11-19 10:48:12 -0700

    Made changes and fixed errors.
    [#133071901]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,6 @@
 class Student < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :cell_phone, :presence => true
-  validates :cell_phone, :length => { :minimum => 12, :maximum => 12 }
-  validates :home_phone, :presence => true
-  validates :home_phone, :length => { :minimum => 12, :maximum => 12 }
-  validates :work_phone, :presence => true
-  validates :work_phone, :length => { :minimum => 12, :maximum => 12 }
-  validates :email, :presence => true
-  validates :email, :length => {:minimum => 4, :maximum => 50}
-  validates :email, :uniqueness => true
+  validates :first_name, :last_name, :cell_phone, :home_phone, :work_phone, :email, presence: true
+  validates :cell_phone, :home_phone, :work_phone, length: { is: 12 }
+  validates :email, length: {:minimum => 4, :maximum => 50}
+  validates :email, uniqueness: true
 end
